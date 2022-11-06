3a1f51fe41633a1fa9d5b96a3d2cbe12bfa39c77
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-06 15:01:03 +0100

    Update binject.c

--- binject.c
+++ binject.c
@@ -1,16 +1,45 @@
-uint32_t find_code_cave(uint32_t cave_size,uint32_t starting_offset,char *buffer,uint32_t size_of_buffer)
+int find_code_cave(uint32_t cave_size,uint32_t starting_offset,char *buffer,uint32_t size_of_buffer)
 {
  uint32_t a,b;
   a=starting_offset;
   
  for(a;a<sizeof(buffer);a++)
   {
-  if(buffer[starting_offset]==0x00)
+   if(buffer[a]==0x00)
   {b+=1;}
    else
   {b=0;}
    if(b==cave_size)
-   {return a;}
-}
+  {return a;}
+   }
  return 0;
+}
+int file_type(char* file)
+{
+ char buf[4];
+ memset(buf,0x00,sizeof(buf));
+ FILE *f
+  f=fopen(file,"r");
+ if(f==NULL)
+  {
+   return 0;
+  }
+ fread(buf,sizeof(char),4,f);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 }
