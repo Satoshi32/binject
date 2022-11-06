60fb0bcc94fde83783b711f10546286248add015
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-06 08:58:12 +0100

    Update util.c

--- util.c
+++ util.c
@@ -3,7 +3,14 @@ uint32_t find_code_cave(uint32_t cave_size,uint32_t starting_offset,char *buffer
  uint32_t a,b;
   a=starting_offset;
   
- for(a;a<sizeof(buffer);
-  {if(buf[starting_offset]==0x00)
-  {i+=1;}else{i=0;}starting_offset
+ for(a;a<sizeof(buffer);a++)
+  {
+  if(buffer[starting_offset]==0x00)
+  {b+=1;}
+   else
+  {b=0;}
+   if(b==cave_size)
+   {return a;}
+}
+ return 0;
 }
