4c3cad9d7904a6ea84617b15d9943b4afbc0804c
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-09 00:34:27 +0100

    Update binject_mach-o.c

--- binject_mach-o.c
+++ binject_mach-o.c
@@ -1,3 +1,10 @@
 int binject_MACH-O(char *file,char *shellcode)
 {
+  uint32_t size;
+  FILE *f;
+  f=fopen(file,"r");
+  fseek(f,0,SEEK_END);
+ size=ftell(f);
+  char *file_buffer = calloc(sizeof(char),size);
+  free(file_buffer);
 }
