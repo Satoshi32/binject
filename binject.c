678f34cfa301d88536f91de6720bf5c1c0c90c74
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-11 19:20:41 +0100

    Update binject.c

--- binject.c
+++ binject.c
@@ -45,6 +45,7 @@ char *apply_suffix_jmp_intel32(char *shellcode,uint32_t shellcodevaddr,uint32_t 
            {
             
            char *jmp_shellcode=calloc(1,strlen(shellcode)+9); 
+            strcat
             
             
             
