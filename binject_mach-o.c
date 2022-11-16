2a09518120eecbca16150135f4d9f476a29ddd9d
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-16 17:00:05 +0100

    Update binject_mach-o.c

--- binject_mach-o.c
+++ binject_mach-o.c
@@ -6,5 +6,12 @@ int binject_MACH-O(char *file,char *shellcode)
   fseek(f,0,SEEK_END);
    size=ftell(f);
    char *file_buffer = calloc(1,size);
+    for(i=0;i<sections;i++)
+  {
+       x=find_code_cave(strlen(shellcode,section_start,section_end,file_buffer);
+                        if(x!=0)
+                        break;
+  }
+                        
   free(file_buffer);
 }
