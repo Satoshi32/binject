673540eb160024505fb67de30c495b4101e533e0
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-16 12:43:53 +0100

    Update binject_elf.c

--- binject_elf.c
+++ binject_elf.c
@@ -1,10 +1,18 @@
 int binject_ELF(char *file,char *shellcode)
-{
+{ 
   uint32_t size;
+  int i =0;
   FILE *f;
   f=fopen(file,"r");
   fseek(f,0,SEEK_END);
  size=ftell(f);
   char *file_buffer = calloc(1,size);
+  for(i=0;i<sections;i++)
+  {
+    
+    
+    
+    
+  }
   free(file_buffer);
 }
