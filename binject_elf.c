2a2249f0859b07f757c7a95b00b6e0fef41c43a8
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-16 12:52:30 +0100

    Update binject_elf.c

--- binject_elf.c
+++ binject_elf.c
@@ -1,7 +1,7 @@
 int binject_ELF(char *file,char *shellcode)
 { 
   uint32_t size;
-  int i =0;
+  int i,x;
   FILE *f;
   f=fopen(file,"r");
   fseek(f,0,SEEK_END);
@@ -9,10 +9,10 @@ int binject_ELF(char *file,char *shellcode)
   char *file_buffer = calloc(1,size);
   for(i=0;i<sections;i++)
   {
-    
-    
-    
-    
+       x=find_code_cave(strlen(shellcode,section_start,section_end,file_buffer);
+                        if(x!=0)
+                        break;
   }
+                        
   free(file_buffer);
 }
