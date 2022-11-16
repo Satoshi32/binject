de3463f70e7a3742d94555d90f00a9a5a617aafd
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-16 17:00:17 +0100

    Update binject_pe.c

--- binject_pe.c
+++ binject_pe.c
@@ -6,5 +6,12 @@ uint32_t size;
   fseek(f,0,SEEK_END);
  size=ftell(f);
   char *file_buffer = calloc(1,size);
+   for(i=0;i<sections;i++)
+  {
+       x=find_code_cave(strlen(shellcode,section_start,section_end,file_buffer);
+                        if(x!=0)
+                        break;
+  }
+                        
   free(file_buffer);  
 }
