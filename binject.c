2ab6714f64c2b9480e534ef1ec036c03f39e1c50
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-19 11:55:44 +0100

    Update binject.c

--- binject.c
+++ binject.c
@@ -3,6 +3,10 @@
 #DEFINE PE 3
 #DEFINE BIG_ENDIAN 1
 #DEFINE LITTLE_ENDIAN 0
+#DEFINE CODE_CAVE 1
+#DEFINE SILVIO_METHOD 2
+#DEFINE DYNAMIC_METHOD 3
+#DEFINE NEW_SECTION 4
 int find_code_cave(uint32_t cave_size,uint32_t starting_offset,uint32_t ending_offset,char *buffer)
 {
  uint32_t a,b;
