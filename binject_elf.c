37a9e035eb4d4a4b69d7461799e81b7267178943
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-11-19 06:45:04 +0100

    Update binject_elf.c

--- binject_elf.c
+++ binject_elf.c
@@ -7,12 +7,35 @@ int binject_ELF(char *file,char *shellcode,int method)
   fseek(f,0,SEEK_END);
  size=ftell(f);
   char *file_buffer = calloc(1,size);
+  if(method==CODE_CAVE)
+  {
   for(i=0;i<sections;i++)
   {
        x=find_code_cave(strlen(shellcode,section_start,section_end,file_buffer);
                         if(x!=0)
                         break;
   }
-                        
+                        if(x!=0)
+                        return 1;
+                        }
+  if(method==SILVIO_METHOD)
+  {
+                          
+                          
+                          
+  
+  }
+  if(method==DYNAMIC_METHOD)
+  {
+  
+    
+    
+    
+    
+    
+    
+    
+    
+  }
   free(file_buffer);
 }
