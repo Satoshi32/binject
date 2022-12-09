b6df2732da46fe7275576b6db19f4bbf66b6a772
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-09 08:57:23 +0100

    Update binject_elf.h

--- binject_elf.h
+++ binject_elf.h
@@ -1,30 +1,29 @@
-#define EI_NIDENT 16
 
 typedef struct {
-        unsigned char   e_ident[EI_NIDENT];
-        Elf32_Half      e_type;
-        Elf32_Half      e_machine;
-        Elf32_Word      e_version;
-        Elf32_Addr      e_entry;
-        Elf32_Off       e_phoff;
-        Elf32_Off       e_shoff;
-        Elf32_Word      e_flags;
-        Elf32_Half      e_ehsize;
-        Elf32_Half      e_phentsize;
-        Elf32_Half      e_phnum;
-        Elf32_Half      e_shentsize;
-        Elf32_Half      e_shnum;
-        Elf32_Half      e_shstrndx;
+        unsigned char   e_ident[16];
+        uint16_t      e_type;
+        uint16_t      e_machine;
+        uint32_t      e_version;
+        uint32_t      e_entry;
+        uint32_t      e_phoff;
+        uint32_t      e_shoff;
+        uint32_t      e_flags;
+        uint16_t      e_ehsize;
+        uint16_t      e_phentsize;
+        uint16_t      e_phnum;
+        uint16_t      e_shentsize;
+        uint16_t      e_shnum;
+        uint16_t      e_shstrndx;
 } Elf32_Ehdr;
 
 typedef struct
 {
-  Elf32_Word    p_type;                 /* Segment type */
-  Elf32_Off     p_offset;               /* Segment file offset */
-  Elf32_Addr    p_vaddr;                /* Segment virtual address */
-  Elf32_Addr    p_paddr;                /* Segment physical address */
-  Elf32_Word    p_filesz;               /* Segment size in file */
-  Elf32_Word    p_memsz;                /* Segment size in memory */
-  Elf32_Word    p_flags;                /* Segment flags */
-  Elf32_Word    p_align;                /* Segment alignment */
+  uint32_t    p_type;                 /* Segment type */
+  uint32_t    p_offset;               /* Segment file offset */
+  uint32_t    p_vaddr;                /* Segment virtual address */
+  uint32_t    p_paddr;                /* Segment physical address */
+  uint32_t    p_filesz;               /* Segment size in file */
+  uint32_t    p_memsz;                /* Segment size in memory */
+  uint32_t    p_flags;                /* Segment flags */
+  uint32_t    p_align;                /* Segment alignment */
 } Elf32_Phdr;
