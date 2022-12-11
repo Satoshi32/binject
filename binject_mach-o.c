bd63e3dbf9d082d24be44f09f8c540b3af5f969f
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-11 19:07:40 +0100

    Update binject_mach-o.c

--- binject_mach-o.c
+++ binject_mach-o.c
@@ -6,6 +6,8 @@ int binject_MACH-O(char *file,char *shellcode)
   fseek(f,0,SEEK_END);
    size=ftell(f);
    char *file_buffer = calloc(1,size);
+	shellcodefixed = ApplySuffixJmpIntel64(shellcodeBytes, uint32(caveOffset), uint32(machoFile.EntryPoint), machoFile.ByteOrder)
+				scfixedlen=strlen(shellcodefixed);
     for(i=0;i<sections;i++)
   {
      
@@ -13,12 +15,13 @@ int binject_MACH-O(char *file,char *shellcode)
 		if (strstr(section.SectionHeader.Seg,"__TEXT") && strstr(section.Name,"__text")) {
 			caveOffset= 0x20;
 			caveOffset+=machoFile.FileHeader.Cmdsz
-			if(shellcode<section.Offset-caveOffset)
-
-			shellcode := api.ApplySuffixJmpIntel64(shellcodeBytes, uint32(caveOffset), uint32(machoFile.EntryPoint), machoFile.ByteOrder)
-			machoFile.Insertion = shellcode
-				memcpy(buf+cave_offset,shellcode,strlen(shellcode);
-			break
+			
+			
+			if(scfixedlen<section.Offset-caveOffset)
+			{
+				memcpy(buf+cave_offset,shellcodefixed,strlen(shellcodefixed);
+			break;
+				       }
 		}
 	}
  
