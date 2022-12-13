b0a5f8243174ebb211c5abc32c84687c136ad20c
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-13 21:42:01 +0100

    Update binject_elf.c

--- binject_elf.c
+++ binject_elf.c
@@ -42,7 +42,24 @@ int binject_ELF(char *file,char *shellcode,int method)
                       return -1;
                         }
   if(method==SILVIO_METHOD)
-  {
+  {/*
+			  Circa 1998: http://vxheavens.com/lib/vsc01.html  <--Thanks to elfmaster
+		        6. Increase p_shoff by PAGE_SIZE in the ELF header
+		        7. Patch the insertion code (parasite) to jump to the entry point (original)
+		        1. Locate the text segment program header
+		            -Modify the entry point of the ELF header to point to the new code (p_vaddr + p_filesz)
+		            -Increase p_filesz to account for the new code (parasite)
+		            -Increase p_memsz to account for the new code (parasite)
+		        2. For each phdr which is after the insertion (text segment)
+		            -increase p_offset by PAGE_SIZE
+		        3. For the last shdr in the text segment
+		            -increase sh_len by the parasite length
+		        4. For each shdr which is after the insertion
+		            -Increase sh_offset by PAGE_SIZE
+		        5. Physically insert the new code (parasite) and pad to PAGE_SIZE,
+					into the file - text segment p_offset + p_filesz (original)
+	*/
+	  
 	  sclen+=5;
 	  x=0;
 	  address+=ehdr->e_phoff
