ca8347315bdf52860008bd23402fdc6a754d1483
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2022-12-16 14:49:08 +0100

    Update binject-util.h

--- binject-util.h
+++ binject-util.h
@@ -9,19 +9,7 @@
 #DEFINE SILVIO_METHOD 2
 #DEFINE DYNAMIC_METHOD 3
 #DEFINE NEW_SECTION 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int find_code_cave(uint32_t cave_size,uint32_t starting_offset,uint32_t ending_offset,char *buffer);
+int file_type(char* file);
+char *ApplySuffixJmpIntel64(char *shellcode,uint32_t cave_offset,uint32_t entry_point,int byte_order);
 #define
